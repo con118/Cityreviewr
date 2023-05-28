9a397d6ae24d7b111f879a82df5b7ce28129da29
Shiby-Mathew <shiby.ajay@gmail.com>
2023-05-28 11:34:44 +1000

    deleted commented codes and modified routes for single city

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -6,21 +6,6 @@ const { User } = require("../../models");
 
 
 
-// router.post("/signup", async (req, res) => {
-//   try {
-//     const dbUserData = await User.create({
-//       username: req.body.username,
-//       email: req.body.email,
-//       password: req.body.password,
-//     });
-
-//     console.log("Successfully added a user");
-//     res.status(200).json(dbUserData);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
 
 // CREATE new user
 router.post('/', async (req, res) => {
@@ -87,39 +72,6 @@ router.post('/login', async (req, res) => {
 });
 
 
-
-// router.post("/login", async (req, res) => {
-//   try {
-//     const dbUserData = await User.findOne({
-//       where: {
-//         email: req.body.email,
-//       },
-//     });
-
-//     if (!dbUserData) {
-//       res.status(400).json({
-//         message: "Incorrect email or password. Please try again!",
-//       });
-//       return;
-//     }
-
-//     const validPassword = await dbUserData.checkPassword(req.body.password);
-
-//     if (!validPassword) {
-//       res
-//         .status(400)
-//         .json({ message: "Incorrect email or password. Please try again!" });
-//       return;
-//     }
-
-//     res
-//       .status(200)
-//       .json({ user: dbUserData, message: "You are now logged in!" });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
 
 // Logout
 router.post('/logout', (req, res) => {
